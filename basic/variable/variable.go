0e71a7efe68a36bc1ccfaa42ce13a6699813255f
Manish Raj <30972053+manishkr108@users.noreply.github.com>
2025-08-04 23:06:21 +0530

    Add hello.go and variable.go example

---
+++ basic/variable/variable.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// 👇 Global variable
+//Use global variables only when necessary (e.g., config, constants, shared state).
+var globalMessage string = "Hello from Global!"
+
+func main() {
+	//Variables declared with var can be used outside functions (package level).
+	//The := short syntax only works inside functions.
+
+	//Using var keyword with type
+
+	var name string = "Manish"
+	age := 25
+	var isAdmin = true
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Admin:", isAdmin)
+
+	// Grouped declaration
+	var (
+		product string = "Manish"
+		price   int    = 25
+		user    bool   = true
+	)
+
+	fmt.Println(product, price, user)
+	fmt.Println(globalMessage)
+}
